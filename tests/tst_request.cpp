edecb4663c9f8f2b59c38069f17ae6b97eb335e9
alirezah95 <alirezahassani95@gmail.com>
2023-01-13 23:59:57 +0330

    feat: initialize tst_request.cpp for testing Request class

---
+++ tests/tst_request.cpp
@@ -0,0 +1,11 @@
+#include <QTest>
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+
+int main(int argc, char* argv[])
+{
+    ::testing::InitGoogleMock(&argc, argv);
+    return RUN_ALL_TESTS();
+}
