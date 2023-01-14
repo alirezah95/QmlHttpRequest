4ff4a94fe87aba6d4ba2f9bd5c2999943862ad90
alirezah95 <alirezahassani95@gmail.com>
2023-01-14 09:58:37 +0330

    docs: add Request class doxycomment

--- src/request.cpp
+++ src/request.cpp
@@ -2,6 +2,16 @@
 
 namespace qhr {
 
+/*!
+ * \class Request
+ * \brief Request call encapsulating a network request and a thin wrapper around
+ * \a\b QNetworkRequest
+ *
+ * The \ref Request class provides methods for opening a request, setting its
+ * headers, sending it, etc with names matching the ones in \a\b
+ * XmlHttpRequest
+ */
+
 Request::Request(QObject* parent) : QObject { parent } { }
 
 void Request::open(const QString& method, const QUrl& url)
