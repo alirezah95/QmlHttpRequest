c808befcb817152487571d7fa4e46d2d16cb1f0f
alirezah95 <alirezahassani95@gmail.com>
2023-01-14 10:03:28 +0330

    docs: add doxycomments for methods

--- src/request.cpp
+++ src/request.cpp
@@ -14,12 +14,26 @@ namespace qhr {
 
 Request::Request(QObject* parent) : QObject { parent } { }
 
+/*!
+ * \qmlmethod open()
+ * \brief Request::open() Opens a request with the \a method as method name and
+ * \a url for request url
+ * \param method The name of the request method, GET, POST, PUT, etc
+ * \param url The url for the request
+ */
 void Request::open(const QString& method, const QUrl& url)
 {
     mMethodName = method;
     mNRequest.setUrl(url);
 }
 
+/*!
+ * \qmlmethod setRequestHeader()
+ * \brief Request::setRequestHeader() Sets the request \a header to \a value.
+ * \note This method must be called after \ref open() and before \ref send()
+ * \param header The name of the header
+ * \param value The value for \a header
+ */
 void Request::setRequestHeader(const QString& header, const QString& value)
 {
     if (!isOpen())
@@ -28,6 +42,12 @@ void Request::setRequestHeader(const QString& header, const QString& value)
     mNRequest.setRawHeader(header.toUtf8(), value.toUtf8());
 }
 
+/*!
+ * \brief Request::requestHeader() Returns header value for \a header. It calls
+ * \a\b QNetworkRequet::rawHeader() internally.
+ * \param header
+ * \return
+ */
 QByteArray Request::requestHeader(const QByteArray& header) const
 {
     return mNRequest.rawHeader(header);
