bcd592ba40bffbb7552aa6fe68a6331589703e7c
alirezah95 <alirezahassani95@gmail.com>
2023-01-14 11:33:45 +0330

    tests: add a test case to test qhr creating a new Request

--- tests/tst_qmlhttprequest.cpp
+++ tests/tst_qmlhttprequest.cpp
@@ -3,11 +3,18 @@
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
+#include "qmlhttprequest.hpp"
+
 class TestQmlHttpRequest : public ::testing::Test
 {
 public:
-    QmlHttpRequest qhr;
+    qhr::QmlHttpRequest qhr;
 };
+
+TEST_F(TestQmlHttpRequest, TestNewRequest)
+{
+    ASSERT_FALSE(qhr.newRequest() == nullptr);
+}
 
 int main(int argc, char* argv[])
 {
